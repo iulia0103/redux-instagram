f67a448f04847c15583d4d0f443e8f2f9deb9f8c
iulia0103 <iulia.leancu@gmail.com>
2016-07-06 21:47:25 +0200

    update the state when the likes are incrementing

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,16 @@
 function posts(state=[], action){
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+    const i = action.index;
+      return [
+        ...state.slice(0,i), //before the one needed for update
+        {...state[i], likes: state[i].likes+1}, //similar to Object.assign()
+        ...state.slice(i+1)//after the one needed
+      ]
+      break;
+    default:
+      return state;
+  }
 }
 
 export default posts;
