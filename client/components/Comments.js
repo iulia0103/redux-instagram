dac57efbd9e094aa97614f815bf6bd2427a15419
iulia0103 <iulia.leancu@gmail.com>
2016-07-07 17:33:10 +0200

    fix onClick

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -13,23 +13,12 @@ const Comments = React.createClass({
     )
   },
 
-  handleSubmit(event){
-    event.preventDefault();
-
-    const {postId} = this.props.params;
-    const author = this.refs.author.value;
-    const comment = this.refs.comment.value;
-
-    this.props.addComment(postId, author, comment);
-    this.refs.commentForm.reset();
-  },
-
   render(){
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
 
-        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+        <form ref="commentForm" className="comment-form">
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
